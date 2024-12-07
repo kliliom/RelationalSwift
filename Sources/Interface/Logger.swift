3e5f6f3bcf92240f57e97593266974305f35ad2d
Kristof Liliom <177391622+kliliom@users.noreply.github.com>
2024-12-07 13:17:03 +0100

    feat: rework logging

---
+++ Sources/Interface/Logger.swift
@@ -0,0 +1,18 @@
+//
+//  Logger.swift
+//  Created by Kristof Liliom in 2024.
+//
+
+@inline(never)
+public func breakOnWarning() {}
+
+private nonisolated(unsafe) var breakOnWarningInfoShown: Bool = false
+
+func warn(_ message: String) {
+    print("[RelationalSwift] WARNING: \(message)")
+    if !breakOnWarningInfoShown {
+        breakOnWarningInfoShown = true
+        print("[RelationalSwift] Set a symbolic breakpoint on `breakOnWarning` to see warnings in the debugger")
+    }
+    breakOnWarning()
+}
